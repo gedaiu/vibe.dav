e151bf68bdce12d43e696a60355c9f6406d9471c
Szabo Bogdan <szabobogdan@yahoo.com>
2015-02-16 00:42:28 +0200

    Updated propfind handler

--- source/vibedav/base.d
+++ source/vibedav/base.d
@@ -16,12 +16,17 @@ import vibe.inet.message;
 import vibe.http.server;
 import vibe.http.fileserver;
 import vibe.http.router : URLRouter;
+import vibe.stream.operations;
+
+import kxml.xml;
 
 import std.conv : to;
 import std.file;
 import std.path;
 import std.digest.md;
 import std.datetime;
+import std.string;
+import std.stdio; //todo: remove this
 
 /// Represents a general DAV resource
 class DavResource {
@@ -30,29 +35,36 @@ class DavResource {
 	HTTPStatus statusCode;
 	bool isCollection;
 
-	string propXmlString() {
+	string propXmlString(bool[string] props = cast(bool[string])[]) {
 		string str = `<d:href>` ~ href ~ `</d:href>`;
 		str ~= `<d:propstat><d:prop>`;
 
 		foreach(key, val; properties) {
-			str ~= `<` ~ key ~ `>` ~ val ~ `</` ~ key ~ `>`;
+			auto key1 = key.toLower;
+
+			if(props.length == 0 || props.length > 0 && (key1 in props) !is null)
+				str ~= `<` ~ key ~ `>` ~ val ~ `</` ~ key ~ `>`;
+
 		}
 
-		if(isCollection)
-			str ~= `<d:resourcetype><d:collection /></d:resourcetype>`;
-		else
-			str ~= `<d:resourcetype />`;
-
+		if(props.length == 0 || (props.length > 0 && ("d:resourcetype" in props) !is null)) {
+			if(isCollection)
+				str ~= `<d:resourcetype><d:collection /></d:resourcetype>`;
+			else
+				str ~= `<d:resourcetype />`;
+		}
 
 		str ~= `</d:prop></d:propstat>`;
 		str ~= `<d:status>HTTP/1.1 ` ~ statusCode.to!string ~ ` ` ~ httpStatusText(statusCode) ~ `</d:status>`;
 
 		return str;
 	}
+
+	abstract DavResource[] getChildren(ulong depth = 1);
 }
 
 /// Represents a file or directory DAV resource
-class DavFileResource : DavResource {
+final class DavFileResource : DavResource {
 	private immutable Path resPath;
 	private immutable Path fileRoot;
 	private immutable Path filePath;
@@ -77,6 +89,7 @@ class DavFileResource : DavResource {
 		}
 
 		auto lastModified = toRFC822DateTimeString(dirent.timeModified.toUTC());
+		auto creationDate = toRFC822DateTimeString(dirent.timeCreated.toUTC());
 		isCollection = pathstr.isDir;
 
 		auto etag = "\"" ~ hexDigest!MD5(pathstr ~ ":" ~ lastModified ~ ":" ~ to!string(dirent.size)).idup ~ "\"";
@@ -84,6 +97,7 @@ class DavFileResource : DavResource {
 		string resType = "";
 		properties["d:getetag"] = etag[1..$-1];
 		properties["d:getlastmodified"] = lastModified;
+		properties["d:creationdate"] = creationDate;
 
 		if(!isCollection) {
 			properties["d:getcontentlength"] = dirent.size.to!string;
@@ -95,7 +109,7 @@ class DavFileResource : DavResource {
 		statusCode = HTTPStatus.OK;
 	}
 
-	DavResource[] getChildren(ulong depth = 1) {
+	override DavResource[] getChildren(ulong depth = 1) {
 		DavResource[] list;
 
 		if(depth == 0) return list;
@@ -129,11 +143,16 @@ struct PropfindResponse {
 	DavResource list[];
 
 	string toString() {
+		bool[string] props;
+		return toStringProps(props);
+	}
+
+	string toStringProps(bool[string] props) {
 		string str = `<?xml version="1.0" encoding="UTF-8"?>`;
 		str ~= `<d:multistatus xmlns:d="DAV:">`;
 
 		foreach(res; list)
-			str ~= `<d:response>` ~ res.propXmlString ~ `</d:response>`;
+			str ~= `<d:response>` ~ res.propXmlString(props) ~ `</d:response>`;
 
 		str ~= `</d:multistatus>`;
 
@@ -142,9 +161,12 @@ struct PropfindResponse {
 }
 
 /// The main DAV protocol implementation
-class Dav {
+abstract class Dav {
 	Path root;
 	enum InfiniteDepth = 99;
+
+	abstract DavResource getResource(Path url);
+	abstract HTTPStatus createCollection(Path url);
 
 	void options(HTTPServerRequest req, HTTPServerResponse res) {
 		string path = req.path;
@@ -157,35 +179,91 @@ class Dav {
 		res.writeBody("", "text/plain");
 	}
 
-	private ulong getDepth(HTTPServerRequest req) {
+	private {
+		ulong getDepth(HTTPServerRequest req) {
 
-		if("depth" in req.headers) {
-			string strDepth = req.headers["depth"];
+			if("depth" in req.headers) {
+				string strDepth = req.headers["depth"];
 
-			if(strDepth == "infinite") return InfiniteDepth;
-			if(strDepth == "1") return 1;
-		} else {
-			return InfiniteDepth;
+				if(strDepth == "infinite") return InfiniteDepth;
+				if(strDepth == "1") return 1;
+			} else {
+				return InfiniteDepth;
+			}
+
+			return 0;
 		}
 
-		return 0;
+		bool[string] propList(XmlNode document) {
+			bool[string] list;
+
+			XmlNode[] prop = document.parseXPath("d:propfind/d:prop");
+
+			if(prop.length > 0) {
+				auto properties = prop[0].getChildren;
+
+				foreach(p; properties)
+					list[p.getName.toLower] = true;
+			}
+			return list;
+		}
 	}
 
 	void propfind(HTTPServerRequest req, HTTPServerResponse res) {
 		string path = req.path;
 		ulong depth = getDepth(req);
+		bool[string] properties;
 
-		auto selectedResource = new DavFileResource(root, Path(path));
+		string requestXml = cast(string)req.bodyReader.readAllUTF8;
+
+		if(requestXml.length > 0) {
+			XmlNode document = readDocument(requestXml);
+			properties = propList(document);
+		}
+
+		auto selectedResource = getResource(Path(path));
 
 		auto response = new PropfindResponse();
 		response.list = selectedResource ~ selectedResource.getChildren(depth);
 
-		res.writeBody(response.toString, "application/xml");
+		res.statusCode = HTTPStatus.multiStatus;
+
+		if(properties.length == 0)
+			res.writeBody(response.toString, "application/xml");
+		else
+			res.writeBody(response.toStringProps(properties), "application/xml");
+
 	}
 
 	void get(HTTPServerRequest req, HTTPServerResponse res) {
 		string path = req.path;
+		res.statusCode = HTTPStatus.ok;
 		sendRawFile(req, res, root ~ path[1..$], new HTTPFileServerSettings);
+	}
+
+	void mkcol(HTTPServerRequest req, HTTPServerResponse res) {
+		string path = req.path;
+
+		res.statusCode = createCollection(Path(path));
+		res.writeBody("", "text/plain");
+	}
+}
+
+/// File dav impplementation
+class FileDav : Dav {
+	override DavResource getResource(Path url) {
+		return new DavFileResource(root, url);
+	}
+
+	override HTTPStatus createCollection(Path url) {
+		auto filePath = root ~ url.to!string[1..$];
+
+		if(filePath.toString.exists)
+			return HTTPStatus.methodNotAllowed;
+
+		mkdir(filePath.toString);
+
+		return HTTPStatus.created;
 	}
 }
 
@@ -199,10 +277,10 @@ HTTPServerRequestDelegate serveDav(T : Dav)(Path path) {
 			dav.options(req, res);
 		} else if(req.method == HTTPMethod.PROPFIND) {
 			dav.propfind(req, res);
-			res.statusCode = HTTPStatus.multiStatus;
 		} else if(req.method == HTTPMethod.GET) {
 			dav.get(req, res);
-			res.statusCode = HTTPStatus.ok;
+		} else if(req.method == HTTPMethod.MKCOL) {
+			dav.mkcol(req, res);
 		} else {
 			res.statusCode = HTTPStatus.notImplemented;
 			res.writeBody("", "text/plain");
@@ -210,10 +288,6 @@ HTTPServerRequestDelegate serveDav(T : Dav)(Path path) {
 	}
 
 	return &callback;
-}
-
-class FileDav : Dav {
-
 }
 
 HTTPServerRequestDelegate serveFileDav(Path path) {
